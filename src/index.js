8b6942900f7657851b70382456153be473d874f3
MishaHryvinskyi <hryvinskyim@gmail.com>
2023-11-11 23:50:01 +0200

    added an equals sign

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const markup = worksItem.map(({ url, name }) => `
   <li class="portfolio__list-item">
     <a href="${url}" class="portfolio__link" data-lightbox="portfolio-images">
       <img class="portfolio__img" src="${url}" alt="${name}" width="300" height="300">
-      <h3 class "portfolio__title">${name}</h3>
+      <h3 class="portfolio__title">${name}</h3>
     </a>
   </li>
 `).join('');
