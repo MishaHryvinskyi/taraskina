229b17229398aa432216a0c23cfb9de21921b83f
MishaHryvinskyi <hryvinskyim@gmail.com>
2023-11-15 01:42:51 +0200

    add paragrap in overlay

--- src/js/markup-portfolio.js
+++ src/js/markup-portfolio.js
@@ -4,16 +4,16 @@ import worksItem from "../db/works.json";
 export const portfolioEl = document.querySelector('.portfolio__list');
 
 export const markup = worksItem.map(({ url, name }) => `
-  <li class="portfolio__list-item">
+    <li class="portfolio__list-item">
     <a href="${url}" class="portfolio__link" data-lightbox="portfolio-images">
-    <div class="portfolio__img-wrapper">
-        <img class="portfolio__img" src="${url}" alt="${name}" width="300" height="300">
-        <div class="portfolio__overlay">
-        <p>${name}</p></div>
-    </div>
-    <h3 class="portfolio__title">${name}</h3>
+        <div class="portfolio__img-wrapper">
+            <img class="portfolio__img" src="${url}" alt="${name}" width="300" height="300">
+            <div class="portfolio__overlay">
+                <p>${name}</p>
+            </div>
+        </div>
+        <h3 class="portfolio__title">${name}</h3>
     </a>
-  </div>
-  </li>
+    </li>
 `).join('');
 
