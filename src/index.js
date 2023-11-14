7b1b9a69ea9051af2f537e150850e1a7c5531577
MishaHryvinskyi <hryvinskyim@gmail.com>
2023-11-14 19:18:27 +0200

    split the js code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
-import worksItem from "../src/db/works.json";
+import { markup, portfolioEl} from '../src/js/markup-portfolio';
+
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
-
-const portfolioEl = document.querySelector('.portfolio__list');
-
-const markup = worksItem.map(({ url, name }) => `
-  <li class="portfolio__list-item">
-    <a href="${url}" class="portfolio__link" data-lightbox="portfolio-images">
-      <img class="portfolio__img" src="${url}" alt="${name}" width="300" height="300">
-      <h3 class="portfolio__title">${name}</h3>
-    </a>
-  </li>
-`).join('');
 
 portfolioEl.innerHTML = markup;
 
--- src/js/markup-portfolio.js
+++ src/js/markup-portfolio.js
@@ -1,1 +1,14 @@
-const portfolioEl = document.getElementById('portfolio');
+import worksItem from "../db/works.json";
+
+
+export const portfolioEl = document.querySelector('.portfolio__list');
+
+export const markup = worksItem.map(({ url, name }) => `
+  <li class="portfolio__list-item">
+    <a href="${url}" class="portfolio__link" data-lightbox="portfolio-images">
+      <img class="portfolio__img" src="${url}" alt="${name}" width="300" height="300">
+      <h3 class="portfolio__title">${name}</h3>
+    </a>
+  </li>
+`).join('');
+
