682dfaf94184917cb59e06fcd933ceb870628710
MishaHryvinskyi <hryvinskyim@gmail.com>
2023-11-14 21:37:50 +0200

    markup and styled overley in portfolio

--- src/js/markup-portfolio.js
+++ src/js/markup-portfolio.js
@@ -6,9 +6,14 @@ export const portfolioEl = document.querySelector('.portfolio__list');
 export const markup = worksItem.map(({ url, name }) => `
   <li class="portfolio__list-item">
     <a href="${url}" class="portfolio__link" data-lightbox="portfolio-images">
-      <img class="portfolio__img" src="${url}" alt="${name}" width="300" height="300">
-      <h3 class="portfolio__title">${name}</h3>
+    <div class="portfolio__img-wrapper">
+        <img class="portfolio__img" src="${url}" alt="${name}" width="300" height="300">
+        <div class="portfolio__overlay">
+        <p>${name}</p></div>
+    </div>
+    <h3 class="portfolio__title">${name}</h3>
     </a>
+  </div>
   </li>
 `).join('');
 
