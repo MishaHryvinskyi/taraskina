4ef7088fac29854a52ccb22023ed82708a9286da
MishaHryvinskyi <hryvinskyim@gmail.com>
2023-11-15 23:29:59 +0200

    add size, name, year in db

--- src/js/markup-portfolio.js
+++ src/js/markup-portfolio.js
@@ -3,13 +3,15 @@ import worksItem from "../db/works.json";
 
 export const portfolioEl = document.querySelector('.portfolio__list');
 
-export const markup = worksItem.map(({ url, name }) => `
+export const markup = worksItem.map(({ url, name, size, year }) => `
     <li class="portfolio__list-item">
     <a href="${url}" class="portfolio__link" data-lightbox="portfolio-images">
         <div class="portfolio__img-wrapper">
             <img class="portfolio__img" src="${url}" alt="${name}" width="300" height="300">
             <div class="portfolio__overlay">
-                <p>${name}</p>
+                <p class="portfolio__paragraph">${name}</p>
+                <p class="portfolio__paragraph">${size}</p>
+                <p>${year}</p>
             </div>
         </div>
         <h3 class="portfolio__title">${name}</h3>
