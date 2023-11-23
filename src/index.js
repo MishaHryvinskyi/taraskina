444d476bfc33a71dd948d33693ef0e9ea85445f5
MishaHryvinskyi <hryvinskyim@gmail.com>
2023-11-23 23:37:40 +0200

    add mobile-menu logic

--- src/index.js
+++ src/index.js
@@ -3,12 +3,35 @@ import 'animate.css';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
-portfolioEl.innerHTML = markup;
+// portfolio
+if (portfolioEl) {
+  portfolioEl.innerHTML = markup;
 
-const portfolioLinks = document.querySelectorAll('.portfolio__link');
+  const portfolioLinks = document.querySelectorAll('.portfolio__link');
 
 const lightbox = new SimpleLightbox(portfolioLinks, {
   captionsData: 'alt',
   captionPosition: 'bottom',
   captionDelay: 250,
 });
+
+}
+
+
+// mobile-menu
+const menuBtnRef = document.querySelector("[data-menu-button]");
+const mobileMenuRef = document.querySelector("[dara-menu]");
+
+
+menuBtnRef.addEventListener("click", () => {
+  const expanded = menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+
+  menuBtnRef.classList.toggle("is-open");
+  menuBtnRef.setAttribute("aria-expanded", !expanded);
+
+  mobileMenuRef.classList.toggle("is-open");
+
+})
+
+
+
