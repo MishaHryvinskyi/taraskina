deec798b295f33fd11ef4b43dd670ae3b6c70f10
Misha <hryvinskyim@gmail.com>
2024-09-01 19:33:30 +0300

    added font

--- src/js/markup-portfolio.js
+++ src/js/markup-portfolio.js
@@ -1,18 +1,18 @@
 function createMarkup(data) {
   const markup = data.map(({ url, name, size, year }) => `
-  <li class="portfolio__list-item">
-    <a href="${url}" class="portfolio__link" data-lightbox="portfolio-images">
-      <div class="portfolio__img-wrapper">
-        <img class="portfolio__img" src="${url}" alt="${name}" width="300" height="300">
-        <div class="portfolio__overlay">
-          <p class="portfolio__paragraph">${name}</p>
-          <p class="portfolio__paragraph">${size}</p>
-          <p>${year}</p>
+    <li class="portfolio__list-item">
+      <a href="${url}" class="portfolio__link" data-lightbox="portfolio-images">
+        <div class="portfolio__img-wrapper">
+          <img class="portfolio__img" src="${url}" alt="${name}" width="300" height="300">
+          <div class="portfolio__overlay">
+            <p class="portfolio__paragraph">${name}</p>
+            <p class="portfolio__paragraph">${size}</p>
+            <p>${year}</p>
+          </div>
         </div>
-      </div>
-      <h3 class="portfolio__title">${name}</h3>
-    </a>
-  </li>
+        <h3 class="portfolio__title">${name}</h3>
+      </a>
+    </li>
 `).join('');
   return markup;
 }
