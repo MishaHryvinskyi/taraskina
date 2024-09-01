eb54d07e2a8c55d39be2ea56cc6c774a8504749c
Misha <hryvinskyim@gmail.com>
2024-09-01 19:01:00 +0300

    fix func markup

--- src/js/markup-portfolio.js
+++ src/js/markup-portfolio.js
@@ -1,19 +1,20 @@
 function createMarkup(data) {
-  return data.map(({ url, name, size, year }) => `
-    <li class="portfolio__list-item">
-      <a href="${url}" class="portfolio__link" data-lightbox="portfolio-images">
-        <div class="portfolio__img-wrapper">
-          <img class="portfolio__img" src="${url}" alt="${name}" width="300" height="300">
-          <div class="portfolio__overlay">
-            <p class="portfolio__paragraph">${name}</p>
-            <p class="portfolio__paragraph">${size}</p>
-            <p>${year}</p>
-          </div>
+  const markup = data.map(({ url, name, size, year }) => `
+  <li class="portfolio__list-item">
+    <a href="${url}" class="portfolio__link" data-lightbox="portfolio-images">
+      <div class="portfolio__img-wrapper">
+        <img class="portfolio__img" src="${url}" alt="${name}" width="300" height="300">
+        <div class="portfolio__overlay">
+          <p class="portfolio__paragraph">${name}</p>
+          <p class="portfolio__paragraph">${size}</p>
+          <p>${year}</p>
         </div>
-        <h3 class="portfolio__title">${name}</h3>
-      </a>
-    </li>
-  `).join('');
+      </div>
+      <h3 class="portfolio__title">${name}</h3>
+    </a>
+  </li>
+`).join('');
+  return markup;
 }
 
 export { createMarkup };
