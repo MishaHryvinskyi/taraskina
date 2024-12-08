ace837d84aef18a9439fd58cbd27671f3fa480e3
Misha <hryvinskyim@gmail.com>
2024-12-08 14:19:38 +0200

    fixed simplelightbox

--- src/js/portfolio.js
+++ src/js/portfolio.js
@@ -4,10 +4,14 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 import { createMarkup } from './markup-portfolio';
 import { getPaintsData } from './API/api';
 
-let currentPage = 1;
-
 const portfolioEl = document.querySelector('.portfolio__list');
 const targetScroll = document.querySelector('.js-guard');
+
+const lightbox = new SimpleLightbox('.portfolio__list a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
 
 let options = {
   root: null,
@@ -15,41 +19,35 @@ let options = {
   threshold: 1.0,
 };
 
+let currentPage = 1;
 let observer = new IntersectionObserver(onLoad, options);
 
 function onLoad(entries, observer) {
   entries.forEach(entry => {
     if(entry.isIntersecting) {
       currentPage += 1;
+
       getPaintsData(currentPage)
       .then(({ data }) => {
         const { paints } = data;
         const { pagination } = data;
+        const currentPage = pagination.currentPage;
+        const totalPages = pagination.totalPages;
+        
         portfolioEl.insertAdjacentHTML('beforeend', createMarkup(paints));
-
-        if (pagination.currentPage === pagination.totalPages) {
+        lightbox.refresh();
+        if (currentPage === totalPages) {
           observer.unobserve(targetScroll);
         }
 
-        const portfolioLinks = document.querySelectorAll('.portfolio__link');
-        const lightbox = new SimpleLightbox(portfolioLinks, {
-          captionsData: 'alt',
-          captionPosition: 'bottom',
-          captionDelay: 250,
-        });
     }).catch(err => console.log(err));
     }
   })
 }
 
-getPaintsData().then(res => {
-    portfolioEl.insertAdjacentHTML('beforeend',createMarkup(res.data.paints));
+getPaintsData().then(({ data }) => {
+  const { paints } = data;
+    portfolioEl.insertAdjacentHTML('beforeend',createMarkup(paints));
     observer.observe(targetScroll);
-
-    const portfolioLinks = document.querySelectorAll('.portfolio__link');
-    const lightbox = new SimpleLightbox(portfolioLinks, {
-      captionsData: 'alt',
-      captionPosition: 'bottom',
-      captionDelay: 250,
-    });
+    lightbox.refresh();
 });
